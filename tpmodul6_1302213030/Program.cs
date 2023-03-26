38d2a08aa4515d4008cf22f4b29b20bcd4e5718d
novitangrh <novitasabila70@gmail.com>
2023-03-27 00:30:17 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213030/Program.cs
+++ tpmodul6_1302213030/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_1302213030;
+
+class main
+{
+    public static void Main(String[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - Novita Sabila Nugraha");
+        video1.printVideoDetails();
+    }
+}
---
+++ tpmodul6_1302213030/SayaTubeVideo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302213030
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random random = new Random();
+            id = random.Next(10000, 99999);
+            playCount = 0;
+
+
+        }
+
+        public void increasePlayCount(int playCount)
+        {
+            this.playCount = playCount;
+    
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID Video     : " + id);
+            Console.WriteLine("Title        : " + title);
+            Console.WriteLine("Play Count   : " + playCount);
+        }
+    }
+ }
+
